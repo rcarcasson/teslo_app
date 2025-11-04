91fa9fbeec2eda4c9e24369c56a884675246f555
Ricardo Carcassón <ricardo.carcasson@gmail.com>
2025-11-03 21:45:56 -0300

    First commit

---
+++ lib/config/config.dart
@@ -0,0 +1,5 @@
+
+
+export 'theme/app_theme.dart';
+export 'constants/environment.dart';
+export 'router/app_router.dart';
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_app/config/config.dart';
+
+void main() async {
+  await Environment.initEnvironment();
+  runApp(
+    ProviderScope(child: MainApp())
+  );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      theme: AppTheme().getTheme(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
